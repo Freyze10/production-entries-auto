1b3253f368adec01ba3a44cd6e009981a74b2961
freyze10 <bjabillanoza@gmail.com>
2026-03-19 11:56:49 +0800

    adding database structure

--- db/schema.py
+++ db/schema.py
@@ -1,7 +1,8 @@
-from connection import connect_db
+from connection import get_connection
 
 def create_table():
-    cursor = connect_db()
+    con = get_connection()
+    cursor = con.cursor()
 
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS tbl_role(
@@ -13,12 +14,50 @@ def create_table():
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS tbl_user(
             user_id SERIAL PRIMARY KEY,
-            role_id INT(2) NOT NULL,
+            role_id INT NOT NULL,
             hostname VARCHAR(36) NOT NULL,
             password VARCHAR(36) NOT NULL, 
             FOREIGN KEY (role_id) REFERENCES tbl_role(role_id)
     """)
 
-    conn.commit()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS tbl_customer(
+            customer_id SERIAL PRIMARY KEY,
+            customer_name VARCHAR(128)
+        )
+    """)
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS tbl_formula_encode(
+            encode_id SERIAL PRIMARY KEY,
+            match_by VARCHAR(128),
+            encoded_by VARCHAR(128),
+            updated_by VARCHAR(128)
+        )
+    """)
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS tbl_formula01(
+            form_id SERIAL PRIMARY KEY,
+            index_no VARCHAR(10),
+            date DATE,
+            customer_id INT NOT NULL,
+            prod_code VARCHAR(12) NOT NULL,
+            prod_color VARCHAR(32) NOT NULL,
+        )
+    """)
+
+
+    con.commit()
     cursor.close()
-    conn.close()
+    con.close()
+
+
+
+
+
+
+
+
+
+
