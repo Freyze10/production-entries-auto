50243cc8047dae0ee08a8a2c0ab942da02c074fb
freyze10 <bjabillanoza@gmail.com>
2026-03-19 09:14:29 +0800

    change allignment of profile to left

--- main.py
+++ main.py
@@ -57,9 +57,10 @@ class MainWindow(QMainWindow):
         profile = QWidget()
         user_profile = QHBoxLayout(profile)
         user_profile.setContentsMargins(10, 0, 0, 0)
-        user_profile.setAlignment(Qt.AlignmentFlag.AlignTop)
+        user_profile.setAlignment(Qt.AlignmentFlag.AlignLeft)
         user_profile.addWidget(QLabel(pixmap=fa.icon('fa5s.user-circle', color="#ecf0f1").pixmap(QSize(40, 40))))
         user_profile.addWidget(QLabel(f"<b>{self.username}</b><br><font color='#bdc3c7'>{self.user_role}</font>"))
+
         sep = QFrame(frameShape=QFrame.Shape.HLine, objectName="Separator")
         sep.setContentsMargins(0, 10, 0, 10)
 
