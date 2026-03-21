ead6b485726cd4a6770bd5d9c38edffea815ea06
freyze10 <bjabillanoza@gmail.com>
2026-03-21 11:00:33 +0800

    updated table model

--- table_model/model.py
+++ table_model/model.py
@@ -1,22 +1,54 @@
-from PyQt6.QtCore import QAbstractTableModel, Qt
+from PyQt6.QtCore import Qt, QAbstractTableModel
+
 
 class TableModel(QAbstractTableModel):
     def __init__(self, data, headers):
         super().__init__()
-        self._data = data
+        self._data = data  # list of dicts, each dict must have a unique 'id'
         self._headers = headers
 
-    def rowCount(self, index):
+    def rowCount(self, parent=None):
         return len(self._data)
 
-    def columnCount(self, index):
+    def columnCount(self, parent=None):
         return len(self._headers)
 
-    def data(self, index, role):
-        if role == Qt.ItemDataRole.DisplayRole:
-            return str(self._data[index.row()][index.column()])
+    def data(self, index, role=Qt.ItemDataRole.DisplayRole):
+        if not index.isValid():
+            return None
 
-    def headerData(self, section, orientation, role):
+        row_data = self._data[index.row()]
+        column_key = self._headers[index.column()]
+
+        if role == Qt.ItemDataRole.DisplayRole:
+            return str(row_data.get(column_key, ""))
+        elif role == Qt.ItemDataRole.UserRole:
+            # Return the unique ID for the row
+            return row_data.get("id")
+        return None
+
+    def headerData(self, section, orientation, role=Qt.ItemDataRole.DisplayRole):
         if role == Qt.ItemDataRole.DisplayRole:
             if orientation == Qt.Orientation.Horizontal:
-                return self._headers[section]
+                return self._headers[section].capitalize()
+            else:
+                return str(section + 1)
+        return None
+
+    def sort(self, column, order):
+        """Sort table by column index."""
+        key = self._headers[column]
+        self.layoutAboutToBeChanged.emit()
+        try:
+            # Attempt numeric sort first
+            self._data.sort(
+                key=lambda x: float(x.get(key, 0)),
+                reverse=(order == Qt.SortOrder.DescendingOrder)
+            )
+        except ValueError:
+            # Fallback to string sort
+            self._data.sort(
+                key=lambda x: str(x.get(key, "")),
+                reverse=(order == Qt.SortOrder.DescendingOrder)
+            )
+        self.layoutChanged.emit()
