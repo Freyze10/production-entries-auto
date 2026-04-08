d36d27b6417d3c4ffa70f25b8b8b1c20c47daa02
freyze10 <bjabillanoza@gmail.com>
2026-04-08 14:44:45 +0800

    call new production only is the prod_id is 0

--- menu/mb_auto_entry.py
+++ menu/mb_auto_entry.py
@@ -315,6 +315,9 @@ class MBAutoEntry(QWidget):
             except Exception as e:
                 QMessageBox.critical(self, "Error", f"Failed to load: {e}")
                 return False
+        else:
+            self.new_production()
+
     def setup_auto_completers(self):
         data = get_all_completer_data()
 
