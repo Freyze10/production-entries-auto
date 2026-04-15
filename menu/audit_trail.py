535194ff0e4ed2c5a19800af176ff827316749f5
freyze10 <bjabillanoza@gmail.com>
2026-04-15 10:38:55 +0800

    created a file for audit trail

---
+++ menu/audit_trail.py
@@ -0,0 +1,9 @@
+import csv
+from datetime import datetime
+
+from PyQt6.QtCore import Qt, QDate
+from PyQt6.QtWidgets import (QWidget, QVBoxLayout, QTableWidget, QTableWidgetItem,
+                             QAbstractItemView, QHeaderView, QMessageBox, QHBoxLayout, QLabel,
+                             QPushButton, QDateEdit, QLineEdit, QFileDialog, QFrame, QGridLayout)
+from PyQt6.QtGui import QFont
+import qtawesome as fa
