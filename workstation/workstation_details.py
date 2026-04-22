fd2e26f14dd7762558e3d621779539d0c141128c
freyze10 <bjabillanoza@gmail.com>
2026-04-22 15:41:42 +0800

    addded the same in the  other pages

--- workstation/workstation_details.py
+++ workstation/workstation_details.py
@@ -1,30 +1,45 @@
-import getpass
-import os
-import re
-import socket
-import uuid
+import psutil
+
+
+def get_real_mac():
+    addrs = psutil.net_if_addrs()
+    stats = psutil.net_if_stats()
+
+    for interface, addr_list in addrs.items():
+        # Skip if interface is down
+        if interface not in stats or not stats[interface].isup:
+            continue
+
+        for addr in addr_list:
+            # AF_LINK = MAC address
+            if addr.family == psutil.AF_LINK:
+                mac = addr.address
+
+                # Skip invalid or virtual MACs
+                if mac and mac != "00:00:00:00:00:00":
+                    # Optional: filter out VMware/VirtualBox
+                    if not mac.lower().startswith(("00:50:56", "00:0c:29", "00:05:69")):
+                        return mac.replace('-', ':')
+
+    return None
 
 
 def _get_workstation_info():
-    # Hostname and IP
+    import socket, os, getpass
+
     try:
-        h, i = socket.gethostname(), socket.gethostbyname(socket.gethostname())
+        h = socket.gethostname()
+        i = socket.gethostbyname(h)
     except:
         h, i = 'Unknown', 'N/A'
 
-    # MAC Address
-    try:
-        m = ':'.join(re.findall('..', '%012x' % uuid.getnode()))
-    except:
-        m = 'N/A'
+    m = get_real_mac() or 'N/A'
 
-    # Local User Account (with fallback)
     try:
         u = os.getlogin()
     except:
         u = getpass.getuser()
 
-    # Combine as "HOSTNAME\Username"
     full_user = f"{h}\\{u}"
 
     return {"h": h, "i": i, "m": m, "u": full_user}
